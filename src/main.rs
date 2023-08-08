0db83df62bc2c67fbbb3ec0e0b23aa137584d3d3
Aldo Pietromatera <aldopietromatera@gmail.com>
2023-08-08 17:08:09 +0200

    cleaned main

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,4 @@
-use petgraph::algo::toposort;
 
 fn main() {
     println!("Hello, world!");
-
-    use petgraph::Graph;
-
-    let mut deps: Graph<&str, &str> = Graph::<&str, &str>::new();
-    let pg = deps.add_node("petgraph");
-    let fb = deps.add_node("fixedbitset");
-    let qc = deps.add_node("quickcheck");
-    let rand = deps.add_node("rand");
-    let libc = deps.add_node("libc");
-    deps.extend_with_edges(&[(pg, fb, "miao"), (pg, qc, "bau"), (qc, rand, "miao"), (rand, libc, "miao"), (qc, libc, "miao")]);
-
-    let toposort = toposort(&deps, None).unwrap();
-
-    for node in toposort{
-        println!("{:?} - {:?}", deps[node], deps.edges_directed(node, petgraph::Direction::Incoming).map(|e| *e.weight()).collect::<Vec<&str>>());
-    }
-
 }
