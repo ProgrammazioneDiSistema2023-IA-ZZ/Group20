8d774c4f980fb581410a53ea8705469fcba4d187
AndrewL73 <leoneandrea7300@gmail.com>
2023-09-01 19:34:52 +0200

    refactor: removed comments

--- onnx-binding/src/lib.rs
+++ onnx-binding/src/lib.rs
@@ -80,11 +80,6 @@ impl PyInferenceOutput {
     }
 }
 
-/// Formats the sum of two numbers as string.
-// #[pyfunction]
-// fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-//     Ok((a + b).to_string())
-// }
 #[pyfunction]
 fn build_service(model_path: PathBuf, num_threads: usize) -> PyResult<PyService> {
     match ServiceBuilder::new(model_path)
