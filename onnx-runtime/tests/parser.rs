7f2106247dffd0264a868ee2671cc7c015071719
attilio-oliva <attilio.oliva@protonmail.com>
2023-09-01 23:39:28 +0200

    refactor(graph): remove useless module

--- onnx-runtime/tests/parser.rs
+++ onnx-runtime/tests/parser.rs
@@ -1,5 +1,6 @@
 use onnx_runtime::onnx_format;
-use onnx_runtime::{graph::load_input, graph::load_output};
+
+use ndarray::{Array2, Array4};
 use prost::Message;
 use std::{
     env,
@@ -10,7 +11,7 @@ use std::{
 
 use onnx_runtime::onnx_format::{
     type_proto::Tensor as TypeTensorProto, type_proto::Value as TypeValueProto, GraphProto,
-    ModelProto, NodeProto, OperatorSetIdProto, TypeProto, ValueInfoProto,
+    ModelProto, NodeProto, OperatorSetIdProto, TensorProto, TypeProto, ValueInfoProto,
 };
 
 #[test]
@@ -124,4 +125,64 @@ fn deserialize_test_data() {
     assert_eq!(resnet_input.shape(), [1, 3, 224, 224]);
     assert_eq!(mobilenet_output.shape(), [1, 1000]);
     assert_eq!(resnet_output.shape(), [1, 1000]);
+}
+
+pub fn load_input(path: &str) -> Array4<f32> {
+    tensor_proto_to_arr4(load_tensor_from_pb(path))
+}
+
+pub fn load_output(path: &str) -> Array2<f32> {
+    tensor_proto_to_arr2(load_tensor_from_pb(path))
+}
+
+fn tensor_proto_to_arr4(tensor: TensorProto) -> Array4<f32> {
+    let [batch_size, chans, height, width] = *(tensor
+        .dims
+        .iter()
+        .map(|&v| v as usize)
+        .collect::<Vec<usize>>())
+    else {
+        todo!("Unexpected format")
+    };
+    let raw_data = tensor
+        .raw_data
+        .expect("Cannot extract raw data from Tensor");
+    let mut f32_data: Vec<f32> = Vec::new();
+    for chunk in raw_data.chunks_exact(4) {
+        let bytes: [u8; 4] = [chunk[0], chunk[1], chunk[2], chunk[3]];
+        let num: u32 = u32::from_le_bytes(bytes);
+        let float_num: f32 = f32::from_bits(num);
+        f32_data.push(float_num);
+    }
+    Array4::from_shape_vec([batch_size, chans, height, width], f32_data).unwrap()
+}
+
+fn tensor_proto_to_arr2(tensor: TensorProto) -> Array2<f32> {
+    let [batch_size, outputs] = *(tensor
+        .dims
+        .iter()
+        .map(|&v| v as usize)
+        .collect::<Vec<usize>>())
+    else {
+        todo!("Unexpected format")
+    };
+    let raw_data = tensor
+        .raw_data
+        .expect("Cannot extract raw data from Tensor");
+    let mut f32_data: Vec<f32> = Vec::new();
+    for chunk in raw_data.chunks_exact(4) {
+        let bytes: [u8; 4] = [chunk[0], chunk[1], chunk[2], chunk[3]];
+        let num: u32 = u32::from_le_bytes(bytes);
+        let float_num: f32 = f32::from_bits(num);
+        f32_data.push(float_num);
+    }
+    Array2::from_shape_vec([batch_size, outputs], f32_data).unwrap()
+}
+
+fn load_tensor_from_pb(path: &str) -> TensorProto {
+    let mut buffer = Vec::new();
+    let mut file = File::open(path).unwrap();
+    file.read_to_end(&mut buffer).unwrap();
+
+    TensorProto::decode(buffer.as_slice()).expect("Unable to read TensorProto")
 }
